89d5c1a7c7392cd01c5ca0a4d40b607ff2832d41
Hennie Bruurmijn <hlm.bruurmijn@gmail.com>
2022-08-13 17:19:02 +0700

    Clean up the code for readability

--- julius.rb
+++ julius.rb
@@ -6,14 +6,14 @@ def caesar_cipher(text, shift = 0)
   end
 
   ascii.map! do |n| # destructive for ease of working
-    unless n.between?(65, 122) # only convert characters
-      n
-    else
+    if n.between?(65, 122)
       if (n < 91 && (n + shift) >= 91) || (n < 123 && (n + shift) >= 123) # if characters overflow correct for this
         n - 26 + shift
       else
         n + shift
       end
+    else # only convert characters
+      n
     end
   end
 
@@ -22,15 +22,15 @@ def caesar_cipher(text, shift = 0)
   puts "Original text was '#{text}'. Shifted by #{shift} letters, the ciphered text becomes '#{new_text}'"
 end
 
-#Show examples
-caesar_cipher("Hello")
-caesar_cipher("Ziggo", 26)
-caesar_cipher("What a string!", 5)
+# Show examples
+caesar_cipher('Hello')
+caesar_cipher('Ziggo', 26)
+caesar_cipher('What a string!', 5)
 
-#Now do it yourself!
-puts "Please input your own text to convert now:"
+# Now do it yourself!
+puts 'Please input your own text to convert now:'
 user_text = gets.chomp
-puts "And how large will your shuffle be tonight? (please enter a number!)"
+puts 'And how large will your shuffle be tonight? (please enter a number!)'
 user_shift = gets.chomp.to_i
 
 caesar_cipher(user_text, user_shift)
